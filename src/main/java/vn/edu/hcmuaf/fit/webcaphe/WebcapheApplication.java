4f8c0c143ad93422bf90022774805515860f885d
nguyenthanhson22130237 <22130237@st.hcmuaf.edu.vn>
2025-05-24 01:06:13 +0700

    git backend

---
+++ src/main/java/vn/edu/hcmuaf/fit/webcaphe/WebcapheApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.hcmuaf.fit.webcaphe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebcapheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebcapheApplication.class, args);
+    }
+
+}
