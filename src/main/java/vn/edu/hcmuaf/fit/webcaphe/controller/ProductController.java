351b5425f3ae93a23dd5ad84fd7b64943c3c7ea4
nguyenthanhson22130237 <22130237@st.hcmuaf.edu.vn>
2025-05-24 22:45:51 +0700

    git backend

--- src/main/java/vn/edu/hcmuaf/fit/webcaphe/controller/ProductController.java
+++ src/main/java/vn/edu/hcmuaf/fit/webcaphe/controller/ProductController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
+@CrossOrigin(origins = "https://resplendent-gumdrop-c65ac7.netlify.app")
 public class ProductController {
 
     @Autowired
